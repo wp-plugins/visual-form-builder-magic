4e0f8e18741cdbd87eaab5beaf809f9b4279a2af
jrevillini <jrevillini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-09 13:03:12 +0000

    features in this release: * add the class "send-to" to any email form field and the submitted information will also get sent to those email addresses
    git-svn-id: https://plugins.svn.wordpress.org/visual-form-builder-magic/trunk@694487 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jrevillini-visual-form-builder-magic.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Plugin Name: Visual Form Builder Magic
+Plugin URI: http://james.revillini.com/visual-form-builder-magic/
+Description: Adds a little magic to the Visual Form Builder (non-pro) plugin. Add class "send-to" to any email field to cause the form to be sent to that address as well.
+Version: 1.0
+Author: James Revillini
+Author URI: http://james.revillini.com
+License: GPL2
+
+    Copyright 2013 James Revillini (email : james@revillini.com)
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class Visual_Form_Builder_Magic {
+	 
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+	
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	public function __construct() {
+	    global $wpdb;
+		
+		// Setup global database table names
+		$this->field_table_name 	= $wpdb->prefix . 'visual_form_builder_fields';
+		
+		// set up hooks
+		add_action( 'init', array( &$this, 'email_setup' ), 9 );	// 9 because we want this to beat VFB's email setup hook
+	} // end constructor
+	
+	/*--------------------------------------------*
+	 * Core Functions
+	 *---------------------------------------------*/
+	
+
+
+	/**
+	 * Handle emailing the content
+	 * 
+	 * @since 1.0
+	 * @uses wp_mail() E-mails a message
+	 */
+	public function email_setup() {
+		add_filter( 'vfb_email_form_settings', array( &$this, 'vfb_filter_form_settings_mods' ), 10, 2 );
+	}
+	
+	function vfb_filter_form_settings_mods( $form_settings, $form_id ){
+		global $wpdb;
+		switch (absint($form_id)) {
+			case 5 :
+				$fields = $wpdb->get_results( $wpdb->prepare( "SELECT field_id FROM $this->field_table_name WHERE form_id = %d AND field_css LIKE '%send-to%'", $form_id ) );
+				foreach ($fields as $field) {
+					if ( isset ($_POST[ "vfb-{$field->field_id}" ]) )
+						$form_settings->form_to[] = $_POST[ "vfb-{$field->field_id}" ];
+				}
+				
+				break;
+		}
+		
+		return $form_settings;
+	}
+  
+	/*--------------------------------------------*
+	 * Private Functions
+	 *---------------------------------------------*/
+ 
+  
+  
+} // end class
+new Visual_Form_Builder_Magic ();
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Plugin Name ===
+Contributors: jrevillini
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QBHP7AUYRQ5S2
+Tags: visual form builder, email
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is an add-on plugin which adds some magic to the wonderful Visual Form Builder (non-pro) plugin.
+
+== Description ==
+
+This is an add-on plugin which adds some magic to the wonderful Visual Form Builder (non-pro) plugin. You must have Visual Form Builder installed for this to do anything. Compatible with VFB 2.7.1 and up.
+
+[Visual Form Builder Magic Plugin Page](http://james.revillini.com/visual-form-builder-magic/)
+
+=== Features ===
+
+*   Add the class send-to to any email form input and the submitted information will also get sent to those email addresses.This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
+
+== Installation ==
+
+1. Install the plugin from your wordpress plugin manager or upload the plugin ZIP file to the plugin manager.
+1. Activate the plugin.
+1. On any Visual Form Builder form, you can now add the class "send-to" to any field's CSS Classes and this plugin will submit the form information to that email as well.
+
+== Frequently Asked Questions ==
+
+= Ask something. =
+
+I will answer you.
+
+== Screenshots ==
+
+1. Add the "send-to" class to the CSS Classes field to cause emails to get sent there as well.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release which supports mutlitple email recipients based on form input
